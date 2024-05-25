0f2828e6215708e32b2f9d7227569062c4eb817c
Ryan <ryan.irani@gmail.com>
2024-05-25 12:18:50 +0100

    updated text

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     },
     server: {
       port: 3000,
-      open: true, // Automatically open the browser
+      open: true, 
     },
     plugins: [react()],
     define: {
